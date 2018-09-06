452efe3356bc35412243c0a864aa596a7bb1675c
Tommy Tran <tommytran@tommys-macbook-pro.local>
2018-09-05 18:01:49 -0700

    Initial Commit

---
+++ MovieFyTests/MovieFyTests.swift
@@ -0,0 +1,36 @@
+//
+//  MovieFyTests.swift
+//  MovieFyTests
+//
+//  Created by Tommy Tran on 05/09/2018.
+//  Copyright © 2018 Tommy Tran. All rights reserved.
+//
+
+import XCTest
+@testable import MovieFy
+
+class MovieFyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
