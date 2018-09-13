2714b970f61defed4f85081188bdcf8c817e95ef
Tommy Tran <tommytran@Tommys-MacBook-Pro.local>
2018-09-12 19:19:15 -0700

    finished

---
+++ MovieFy/View/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  MovieFy
+//
+//  Created by Tommy Tran on 12/09/2018.
+//  Copyright © 2018 Tommy Tran. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
